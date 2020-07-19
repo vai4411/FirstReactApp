00d17e352ea908cc62b3a9d88d2e72a73509483e
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-07-19 19:57:40 +0530

    ADDED: used price for display Pricing description

---
+++ src/Price.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function price() {
+  return (
+    <div>
+        <h1>Pricing</h1>
+        <p>Quickly build an effective pricing table for your potential</p>
+        <p>customers with this layout. It's built with default</p>
+        <p>Material-UI components with little customization.</p>
+    </div>
+  );
+}
+
+export default price;
