2c86fc14e663a55150e05e1d0fc49789990b6d90
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-07-19 19:57:40 +0530

    ADDED: used price for display Pricing description

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import TopBar from './topBar';
+import Price from './price';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TopBar />
+    <Price />
   </React.StrictMode>,
   document.getElementById('root')
 );
