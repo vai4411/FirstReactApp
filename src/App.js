337fd9502ae376aaabd1278c1a54eb6f48e654a3
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-07-19 17:54:34 +0530

    ADDED: used function App for create basic top bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h3>Company name</h3>
+        <label>FATURES</label>
+        <label>ENTERPRISE</label>
+        <label>SUPPORT</label>
+        <button>LOGIN</button>
     </div>
   );
 }
