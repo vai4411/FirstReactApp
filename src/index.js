69075cc1c61fc1a228bd4cf4ad4c792e66ae3583
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-07-20 00:19:53 +0530

    ADDED: Used about for display additional information

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import TopBar from './topBar';
 import Price from './price';
 import Subs from './plans';
+import Details from './about';
 
 ReactDOM.render(
   <React.StrictMode>
     <TopBar />
     <Price />
     <Subs />
+    <Details />
   </React.StrictMode>,
   document.getElementById('root')
 );
